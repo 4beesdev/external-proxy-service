c8082e55e31ea6c5a22a680f5039528f9db9d0dc
MilicaV10 <milica.vukovljak001@gmail.com>
2025-12-18 12:39:19 +0100

    Initial commit

---
+++ src/main/java/com/mone/backend/external_proxy_service/ExternalProxyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mone.backend.external_proxy_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalProxyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mone/backend/external_proxy_service/ExternalProxyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mone.backend.external_proxy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalProxyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
